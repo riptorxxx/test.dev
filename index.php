0bdfc1c38c6732f49d0ca4a91fd47d232d36db0b
sggr <sggr90@gmail.com>
2014-03-13 04:50:14 +0400

    header, content стили

--- index.php
+++ index.php
@@ -3,18 +3,55 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="/css/header.css">
     <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <link rel="stylesheet" type="text/css" href="/css/header.css">
 </head>
 <body>
     <!-- Шапка сайта -->
     <header>
-
+        <ul id="nav">
+            <a href="#">
+                <li>Lorem</li>
+            </a>
+            <a href="#">
+                <li>Excepturi</li>
+            </a>
+            <a href="#">
+                <li>Ad</li>
+            </a>
+            <a href="#">
+                <li>Incidunt</li>
+            </a>
+            <a href="#">
+                <li>Delectus</li>
+            </a>
+        </ul>
     </header>
 
     <!-- Контент -->
-    <section id="wrapper">
-
+    <section id="content">
+        <section id="left">
+            <ul>
+                <a href="">
+                    <li>Lorem</li>
+                </a>
+                <a href="#">
+                    <li>Dolorem</li>
+                </a>
+                <a href="#">
+                    <li>Praesentiu</li>
+                </a>
+                <a href="#">
+                    <li>Eveniet</li>
+                </a>
+                <a href="#">
+                    <li>Tempora</li>
+                </a>
+            </ul>
+        </section>
+        <section id="right">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa ipsam labore modi perferendis? Deleniti dolore ex harum, laborum modi sequi vitae. Aspernatur aut consectetur dolore, doloribus nostrum perferendis ratione? Ad aut hic illum nostrum placeat? Consectetur consequatur corporis culpa debitis, dicta dolor doloremque, dolores dolorum ducimus explicabo id illo iusto labore laborum, magnam natus neque nihil nobis nulla obcaecati officia pariatur perspiciatis rem tenetur totam veniam vitae voluptatem! Est illum iste numquam quidem ut vitae. A animi deleniti ducimus error itaque laboriosam mollitia quaerat recusandae, rem repudiandae tenetur, ut! Ab dignissimos incidunt maxime nemo non numquam quia repudiandae totam!
+        </section>
     </section>
 
     <!-- Подвал сайта -->
