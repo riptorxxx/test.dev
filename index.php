4f7013269d39843d03a046bad3e871e8fa689d13
sggr <sggr90@gmail.com>
2014-03-13 04:15:10 +0400

    Базовый шаблон

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
-sggr228
-riptor228
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Шапка сайта -->
+    <header>
+
+    </header>
+
+    <!-- Контент -->
+    <section id="wrapper">
+
+    </section>
+
+    <!-- Подвал сайта -->
+    <footer>
+
+    </footer>
+</body>
+</html>
