1069be4b7693d9889b1aafbf4cfc505ff493d22e
riptorxxx <riptor@bk.ru>
2014-03-13 04:19:52 +0400

    Подключил файл css стилей для footer

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="/css/footer.css">
 </head>
 <body>
     <!-- Шапка сайта -->
